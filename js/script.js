27628b816062da94e05487585d663a6d3eeb683d
Ivonne Chen <starwithu@gmail.com>
2014-06-11 18:28:25 +0800

    img

--- js/script.js
+++ js/script.js
@@ -108,7 +108,7 @@ FB.getLoginStatus(function(response) {
 			//canvas.height = profileIMG.height;
 			ctx.drawImage(img,0,0); //劃入img
 			ctx.drawImage(profileIMG,canMouseX-128/2,canMouseY-120/2);//從XY軸0，0值開始畫如profileimg
-			ctx.drawImage(img3,0,0); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+			ctx.drawImage(img3,img.width/2,img.height); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
